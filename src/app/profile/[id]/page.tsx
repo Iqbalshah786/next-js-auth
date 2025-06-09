d3b69c8813ec8a99dfc1a55da40fbe60ebdc3d68
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-06-09 15:53:16 +0500

    refactor profile page to use async params and enhance user details retrieval with logout functionality

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,9 @@
-export default function UserProfile({ params }: any) {
+// enable dynamic, since we're reading route params
+export const dynamic = "force-dynamic";
+
+export default async function UserProfile({ params }: { params: Promise<{ id: string }> }) {
+  // params is now async, so await it
+  const { id } = await params;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center">
       <h1>Profile</h1>
@@ -6,7 +11,7 @@ export default function UserProfile({ params }: any) {
       <p className="text-4xl mt-3">
         Profile
         <span className="ml-4 p-2 rounded bg-orange-500 text-black">
-          {params.id}
+          {id}
         </span>
       </p>
     </div>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,58 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import toast from "react-hot-toast";
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const [data, setData] = React.useState<any>("nothing");
+
+  const onLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful!");
+      // Redirect to login page after logout
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Error logging out:", error);
+      toast.error(
+        error.response?.data?.error || "Something went wrong while logging out!"
+      );
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log("User Details:", res.data?.user?._id);
+    setData(res.data?.user?._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center">
       <h1>Profile</h1>
       <hr />
-      <p>Profile</p>
+      <p>Profile page</p>
+      <h2 className="rounded bg-green-500 p-4 text-white mt-4">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <button
+        onClick={onLogout}
+        className="bg-blue-500 hover:cursor-pointer hover:bg-blue-600 mt-4 text-white rounded-md p-2 mb-4"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-500 hover:cursor-pointer hover:bg-green-600 mt-4 text-white rounded-md p-2 mb-4"
+      >
+        Get User Details
+      </button>
     </div>
   );
 }
