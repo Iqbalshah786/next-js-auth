940a794291f3093a04ed3c340d12c36d34cb05b7
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-06-10 13:40:32 +0500

    refactor email sending logic for reset password functionality and improve logging

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -16,33 +16,48 @@ export const sendEmail = async ({email,emailType,userId}:any) => {
     }else if(emailType === "RESET"){
         await User.findByIdAndUpdate(userId, {
         forgotPasswordToken: hashedToken,
-        forgotPasswordTokenExpiry: new Date(Date.now() + 3600000)})  
+        forgotPasswordExpiry: new Date(Date.now() + 3600000)})  
     }
 
- const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
-  auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-  },
-});
+    // choose transport based on environment
+    const isProduction = process.env.NODE_ENV === 'production';
+    const transporter = isProduction
+      ? nodemailer.createTransport({
+          service: process.env.EMAIL_SERVICE,
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+          },
+        })
+      : nodemailer.createTransport({
+          host: process.env.SMTP_HOST,
+          port: Number(process.env.SMTP_PORT),
+          secure: false,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+          },
+        });
 
  const mailOptions = {
     from: process.env.SMTP_FROM,
     to: email,
     subject: emailType === "VERIFY" ? "verify email":"Reset your Password",
-    html:`<p>Click  <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}.</p>
-    <p>or copy the link <br />${process.env.DOMAIN}/verifyemail?token=${hashedToken} and paste in the browser</p>
-    `
+    html:
+         `${emailType === "VERIFY" ?
+         `<p>Click  <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}.</p>
+    <p>or copy the link and paste in the browser<br />${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`: 
+    
+    `<p>Click  <a href="${process.env.DOMAIN}/resetpassword?token=${hashedToken}">here</a> to reset your password.</p> <br />
+    <p>or copy the link and paste in the browser<br />${process.env.DOMAIN}/resetpassword?token=${hashedToken}</p>`}
+        `
   };
 
   const mailresponse = await transporter.sendMail(mailOptions);
 
-  console.log('Welcome email sent successfully');
+  console.log('Email sent successfully');
   } catch (error:any) {
-    console.error('Error sending welcome email:', error);
+    console.error('Error sending email:', error);
     throw new Error(`Failed to send email: ${error.message}`);
 
   }
