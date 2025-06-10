cd12fe1310c02e434feb0957d25e6ec30fd010b3
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-06-10 16:20:39 +0500

    add @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
         {/* global toast container */}
         <Toaster position="top-right" />
         {children}
+        <Analytics />
       </body>
     </html>
   );
